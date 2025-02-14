c6c09c9873e5efecf73e0fb5a449a02987e5e886
prasanna <prasannasiri531@gmail.com>
2025-02-14 18:35:09 -0500

    java

---
+++ src/main/java/com/example/DemoController.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DemoController {
+
+    @GetMapping
+    public String home() {
+        return "Hello! Your Spring Boot application is running successfully!";
+    }
+}
